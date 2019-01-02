ea0468333f12dc1e18aeef4dd2e6472c9747c5bf
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-01-02 22:24:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,14 +4,13 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-  
-
+  projects = {}  
 
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
   
-  binding.pry
+  # binding.pry
 
 end
 
